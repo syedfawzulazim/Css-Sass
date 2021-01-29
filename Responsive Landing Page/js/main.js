acc79118b5881a78e3420f64e6a8a94637686f94
Syed Fawzul Azim <adnansyed27@gmail.com>
2021-01-29 23:28:14 +0600

    Hero Section Added

--- Responsive Landing Page/js/main.js
+++ Responsive Landing Page/js/main.js
@@ -1,16 +1,25 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function(){
     if(header.classList.contains('open')){
+        body.classList.remove('noscroll')
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function(element){
+        element.classList.remove('fade-in');
+        element.classList.add('fade-out');
+        });
     }
     else{
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+
     }
 });
