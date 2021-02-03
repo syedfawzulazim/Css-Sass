e7c5b6f39c5fb8a357222741fc791297d21fe1c4
Syed Fawzul Azim <adnansyed27@gmail.com>
2021-02-03 16:51:12 +0600

    Navbar Done

---
+++ Responsive Navbars/js/main.js
@@ -0,0 +1,15 @@
+const menuBar = document.querySelector(".header__menu");
+
+let firstTime = 0;
+
+
+function openMenu(){
+
+    if(menuBar.classList.contains('hide-for-mobile-table') )
+    {
+        menuBar.classList.remove('hide-for-mobile-table');
+    }
+    else{
+        menuBar.classList.add('hide-for-mobile-table');
+    }
+}
