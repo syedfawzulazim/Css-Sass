dce9c426c3164954d748dc7bcac9fcc26565d02b
Syed Fawzul Azim <adnansyed27@gmail.com>
2021-02-12 06:42:47 +0600

    Responsive Done

--- Sparrow'S Website/js/main.js
+++ Sparrow'S Website/js/main.js
@@ -1,7 +1,14 @@
 const headerToggle = document.querySelector('.header__toggle');
+const headerNav = document.querySelector('.header__nav');
 
 headerToggle.addEventListener('click', () =>{
    headerToggle.classList.toggle("active");
+  if(headerToggle.classList.contains('active')){
+   headerNav.style.left = "0%";
+  }
+  else{
+   headerNav.style.left = "-60%";
+  }
 })
 
 
